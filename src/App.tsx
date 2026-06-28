f7db0cbc72769d3e348623df20f66c18b788ba2d
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-06-28 13:49:04 +0300

    sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import ChevronRight from '~/assets/chevron-right.svg?react'
 import Logo from '~/assets/kvilon-logo.svg?react'
 import config from '~/config'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [scroll, setScroll] = useState(false)
+
+  useEffect(() => {
+    function handler() {
+      const scrollTop = window.scrollY
+      if (scrollTop > 48) {
+        setScroll(true)
+      } else {
+        setScroll(false)
+      }
+    }
+    window.addEventListener('scroll', handler)
+
+    return () => {
+      window.removeEventListener('scroll', handler)
+    }
+  }, [])
+
   return (
     <div className="grid min-h-screen grid-cols-[minmax(0,1fr)_minmax(auto,1780px)_minmax(0,1fr)] grid-rows-[auto_1fr] gap-x-4 bg-neutral-100">
-      <header className="col-2 flex items-center justify-between rounded-full bg-dark-stone p-4 px-6 text-white">
+      <header
+        className={`${scroll ? 'bg-dark-stone/85' : 'bg-dark-stone'} sticky top-2 z-10 col-2 
+          my-2 flex items-center justify-between rounded-full bg-dark-stone p-4 px-6 text-white backdrop-blur-md transition-colors duration-700`}
+      >
         <Logo className="w-40" />
 
         <nav className="font-extra">
@@ -120,7 +142,7 @@ const App = () => {
             <div>Designed &amp; developed by me</div>
           </div>
 
-          <div className="col-2 row-[1/-1] grid grid-cols-3 grid-rows-subgrid">
+          <div className="col-2 row-span-full grid grid-cols-3 grid-rows-subgrid">
             {config.footerSections.map((section) => (
               <section className="row-span-full grid grid-rows-subgrid justify-items-center border-l border-bronze-700 last:border-r">
                 <div className="row-2 grid content-start gap-7">
